38958d661122f5ee99b70c0fe830de76f5360d60
0xseven7 <zhangxu1161@gmail.com>
2022-11-02 20:08:38 +0800

    nm

--- pages/components/User/Transfer.tsx
+++ pages/components/User/Transfer.tsx
@@ -11,7 +11,7 @@ export default function Transfer() {
         <div><Box className='' w="500px" my={2} >
             <div>Transfer test</div>
             <Input value={address} onChange={(e) => { setAddress(e.target.value) }} className='' mr={10} my={2} placeholder={'Please input an Ethereum address'}></Input>
-            <Input value={balance} onChange={(e) => { setBalance(Number(e.target.value)) }} className='' mr={10} my={2} placeholder={'Please input balance'}></Input>
+            <Input value={balance} onChange={(e) => { setBalance(Number(e.target.value)) }} mr={10} my={2} placeholder={'Please input balance'}></Input>
             <Button onClick={handleTransfer}>Transfer</Button>
             <div>Transfer is not available</div>
         </Box>
